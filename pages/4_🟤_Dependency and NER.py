37322463d2a5607c5aa4d7692b52c248e1913b43
Sergio Calvo <116919168+Sercalvo@users.noreply.github.com>
2023-05-12 14:54:37 +0200

    Update 4_🟤_Dependency and NER.py

--- pages/4_🟤_Dependency and NER.py
+++ pages/4_🟤_Dependency and NER.py
@@ -30,38 +30,52 @@ st.markdown("Perform real-time analysis of a text to extract features and see a 
 nlp_en = spacy.load("en_core_web_md")
 nlp_es = spacy.load("es_core_news_sm")
 
+# Add a header for the first section: Select text
+st.header("Add your text to analyze")
+
+
 # Use the st.radio function to create a radio button for the user to choose between Spanish and English
-language = st.radio("**Choose a language**", ("English", "Spanish"))
+language = st.radio("Choose a language:", ("English", "Spanish"))
 
 # Use an if statement to select the appropriate language model based on the user's selection
 if language == "English":
     nlp = nlp_en
+    sample_data = {
+        "Text 1": "Chuck Norris doesn't churn butter. He roundhouse kicks the cows and the butter comes straight out. When the Boogeyman goes to sleep every night, he checks his closet for Chuck Norris CNN was originally created as the 'Chuck Norris Network' to update Americans with on-the-spot ass kicking in real-time.",
+        "Text 2": "Google announced a host of new artificial intelligence features coming to its products and services at the company’s annual I/O developer conference yesterday (May 10), where CEO Sundar Pichai mentioned the term “A.I.” 27 times during his 15-minute keynote, per Observer’s count. (Other executives mentioned “A.I.” about 100 additional times over the two-hour event.)"
+    }
 else:
     nlp = nlp_es
+    sample_data = {
+        "Text 1": "Chuck Norris no cree que haya 50 estados en Estados Unidos. Sólo hay uno: Estado de emergencia. Allá por donde pasa es una emergencia. Chuck Norris hace el desayuno de los campeones. Literalmente. Se comió a Fernando Alonso, Tiger Woods y a Roger Federer en una sola comida.",
+        "Text 2": "La inteligencia artificial es la gran apuesta de las principales compañías tecnológicas, y Google acaba de dejar claro que está dispuesta a sacarle el mayor provecho. En su conferencia anual Google I/O en Mountain View, California, el CEO de la Alphabet, Sundar Pichai, habló del objetivo de la compañía para hacer la inteligencia artificial «útil para todos», presentando varios ejemplos de cómo esta tecnología mejorará varios de sus productos."
+    }
 
 
+# get text input from user
+input_type = st.radio('Choose input type:', ['Paste text', 'Select sample data', 'Upload file'])
 
-# Create a dictionary containing example text in Spanish and English
-examples = {
-    "Spanish": "¡Hola! Me llamo Sergio. Vivo en España y trabajo como traductor, pero lo que realmente me encanta es investigar en PLN.",
-    "English": "Hello, my name is Sergio. I live in Spain and I work as a translator, but my real passion is researching in NLP.",
-}
-
-# Use the st.checkbox function to create a checkbox that the user can use to load example data
-if st.checkbox("Load example data"):
-    # Set the text in the text area to the example text for the selected language
-    text = examples[language]
+if input_type == 'Paste text':
+    text = st.text_area('Enter text to analyze')
+elif input_type == 'Select sample data':
+    
+    selected_sample = st.selectbox('Select sample data', list(sample_data.keys()))
+    text = sample_data[selected_sample]
 else:
-    # Otherwise, allow the user to enter their own text
-    text = st.text_area("Enter some text:")
+    uploaded_file = st.file_uploader('Upload file', type=['txt'])
+    if uploaded_file is not None:
+        text = uploaded_file.read().decode('utf-8')
+    else:
+        text = ''
 
 
-# st.text_input takes a label and default text string:
-#input_text = st.text_input("Text string to analyze:", "On April 23d, when Brian Blinking-Eye, the Big Love Company CEO, was signing an agreement to share 3 million love cards with the world, they met in Paris for the G-23 Conference. That morning, they agreed to meet around 11pm at night, to enjoy a great concert featuring Bagus & Giulia Rock where they got lost in the crowd.")
-# Send the text string to the SpaCy nlp object for converting to a 'doc' object.
 doc = nlp(text)
 
+
 if text:
+    st.subheader('Text to analyze')
+    #st.write(text)
+    st.markdown(f":green[{text}]")
     # Add a section header:
     st.header("Named Entity Recognition visualizer")
     # Take the text from the input field and render the entity html.
