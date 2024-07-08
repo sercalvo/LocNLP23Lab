69a2316c96d5173ea9786cec07183a9eaead0c67
Sergio Calvo <116919168+Sercalvo@users.noreply.github.com>
2024-07-08 14:40:22 +0200

    Fix errors

--- vfunctions.py
+++ vfunctions.py
@@ -94,6 +94,7 @@ def show_magic_dataframe(df):
     
         # Display the selected columns in the table
         st.write(df[selected_columns])
+        #st.write(df[selected_columns].T)  # Transpose the DataFrame
         
         
 
